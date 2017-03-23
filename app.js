4c48ca3e2b143ffe7a11a6519202e76fc70b2d79
Agustin Bally <abally@plataforma.com>
2017-03-23 00:30:35 -0300

    creo rutas de la api, invoco métodos del controller

--- app.js
+++ app.js
@@ -4,18 +4,48 @@ var express = require('express'),
   methodOverride = require('method-override'),
   mongoose = require('mongoose');
 
+// Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-var router = express.Router();
+// Import Models and controllers
+var models     = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
 
-router.get('/', function(req, res){
-  res.send("Hola Agustin!");
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.send("Hello world!");
 });
 
 app.use(router);
 
-app.listen(3000, function(){
-  console.log("node-server running in port 3000")
+// API routes
+var tvshows = express.Router();
+
+// tvshows.route('/tvshows')
+//   .get(TVShowCtrl.findAllTvShows)
+//   .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
+app.listen(8099, function() {
+  console.log("Node server running on http://localhost:8099");
 });
